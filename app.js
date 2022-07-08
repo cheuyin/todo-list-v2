ba364318e6b5d87f2f997785e9e06ae7403c8877
Yin <yinstanleycheung@gmail.com>
2022-07-08 15:50:12 -0700

    'New item' button adds item to todolistDB

--- app.js
+++ app.js
@@ -56,15 +56,13 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const newItem = new Item({name: itemName});
+
+  newItem.save();
+  
+  res.redirect("/");
 });
 
 app.get("/work", function(req,res){
